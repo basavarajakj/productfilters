ee4a608ca104a9f9443923e1d3fb76d49c5f6770
Basavarajakjbasuu <basavarajakj06@gmail.com>
2025-01-21 22:32:57 +0530

    Traffic light 🚦🚦signal simulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import Image from "next/image";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { ChevronDownIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <main>
-      
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
+         
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900">
+          High-quality cotton selection
+        </h1>
+
+        <div className="flex items-center">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="group inset-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
+              Sort
+              <ChevronDownIcon className="-mr-1 ml-1"
+            </DropdownMenuTrigger>
+          </DropdownMenu>
+        </div>
+      </div>
     </main>
   );
 }
